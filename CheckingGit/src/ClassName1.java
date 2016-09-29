a78374261151d333b685b24b0834d6166475006e
n.kumarasamy <n.kumarasamy@bdc4b-d-740mx52.dir.svc.accenture.com>
2016-09-29 14:13:39 +0530

    Updated Code

--- CheckingGit/src/ClassName1.java
+++ CheckingGit/src/ClassName1.java
@@ -4,5 +4,6 @@ public class ClassName1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("The program is created");
+		System.out.println("The program is created");	
 	}
 }
